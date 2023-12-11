eb36f7abfa7bf8106183d4ff0b609155321488a8
Muhafidz Ahmad Halim <muhafidz.ahmad@gmail.com>
2023-12-11 08:15:34 +0700

    Day 21

---
+++ Day 021 (6 Kyu - Stop gninnipS My sdroW)/spin_words.js
@@ -0,0 +1,8 @@
+function spinWords(string){
+    return string.split(" ").map(function (word) {
+      if (word.length >= 5) {
+        return word.split("").reverse().join("")
+      }
+      return word
+    }).join(" ")
+  }
