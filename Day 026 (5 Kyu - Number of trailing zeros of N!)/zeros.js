0e6f90c87a4e3af532695b72dbc953a18900db2b
Muhafidz Ahmad Halim <muhafidz.ahmad@gmail.com>
2023-12-16 20:36:48 +0700

    Day 26

--- Day 026 (5 Kyu - Number of trailing zeros of N!)/zeros.js
+++ Day 026 (5 Kyu - Number of trailing zeros of N!)/zeros.js
@@ -1,8 +1,8 @@
-function zeros (n) {
-    let result = 0
-    while (n > 1) {
-      n = parseInt(n/5)
-      result += n
-    }
-    return result
+function zeros(n) {
+  let result = 0
+  while (n > 1) {
+    n = parseInt(n / 5)
+    result += n
   }
+  return result
+}
